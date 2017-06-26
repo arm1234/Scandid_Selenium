705788dd5b388f033152e6f6f6ad18efec2e2a58
arm1234 <anurag.mundra99@gmail.com>
2017-06-26 14:11:29 +0530

    Add files via upload

---
+++ Examples/Test.java
@@ -0,0 +1,77 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://scandid.in/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void test() throws Exception {
+    driver.get(baseUrl + "/coupon");
+    driver.findElement(By.linkText("View all Categories")).click();
+    driver.findElement(By.cssSelector("img[alt=\"Store Logo \"]")).click();
+    driver.findElement(By.linkText("See Coupon")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp |  | 30000]]
+    driver.findElement(By.cssSelector("li.selected")).click();
+    driver.findElement(By.id("switch__input_1")).click();
+    driver.findElement(By.id("classBTN__input_1")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
